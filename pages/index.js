e6467db4f2ea321d8e0f8e144f78c15c0ae2c55b
kentoda <kent19950811@gmail.com>
2021-02-07 22:37:36 +0900

    shopify with Next.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Layout, Page, TextStyle } from "@shopify/polaris";
+const Home = () => {
+  return (
+    <Page>
+      <Layout>
+        <TextStyle variation="positive">
+          Sample app using React and Next.js
+        </TextStyle>
+      </Layout>
+    </Page>
+  );
+};
+
+export default Home;
